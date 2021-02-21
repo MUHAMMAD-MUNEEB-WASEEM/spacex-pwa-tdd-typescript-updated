22a93668642421f15db8e4f8ff8599987091aa2c
Muhammad Muneeb Waseem <50763652+MUHAMMAD-MUNEEB-WASEEM@users.noreply.github.com>
2021-02-21 10:48:07 +0500

    firebase deployment

---
+++ public1/build/precache-manifest.5e0197bb930429e8792bbec5bd8c2690.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff483d4713f4513ad779e2b49baf2d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6c839d2d2196bfdc28b",
+    "url": "/static/css/main.678bcff0.chunk.css"
+  },
+  {
+    "revision": "6517edf0227848b32849",
+    "url": "/static/js/2.80bb3329.chunk.js"
+  },
+  {
+    "revision": "abfa9001b4087bb9e9a892e4db297d32",
+    "url": "/static/js/2.80bb3329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6c839d2d2196bfdc28b",
+    "url": "/static/js/main.2316fa20.chunk.js"
+  },
+  {
+    "revision": "a9c253905645287a4bf2",
+    "url": "/static/js/runtime-main.a04d4aa3.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
