7b88a3aaa5427d377b23982ccf8cfdb00cb02950
Muhammad Muneeb Waseem <50763652+MUHAMMAD-MUNEEB-WASEEM@users.noreply.github.com>
2021-02-21 10:46:02 +0500

    firebase deployment

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,16 @@
+importScripts('https://www.gstatic.com/firebasejs/8.0.1/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/8.0.1/firebase-messaging.js')
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDLFHsnGAJkdDyCWCAQmgpQer3WbuREj-o",
+  authDomain: "spacex-pwa-1398e.firebaseapp.com",
+  projectId: "spacex-pwa-1398e",
+  storageBucket: "spacex-pwa-1398e.appspot.com",
+  messagingSenderId: "208399948410",
+  appId: "1:208399948410:web:6a1e675ccdc01266d5f9c3",
+  measurementId: "G-ZM5QG0B9H0"
+
+  })
+
+firebase.messaging();
